3599e181739d14d1dafb579de0f8323de7ce6530
Dylan Sewell <contact@dylanswl.com>
2022-09-16 08:04:25 -0700

    analytics lecture

--- SingletonIssuesTests/SingletonIssuesTests.swift
+++ SingletonIssuesTests/SingletonIssuesTests.swift
@@ -8,29 +8,66 @@
 import XCTest
 @testable import SingletonIssues
 
+class Tracker {
+    
+    let analytics: Analytics
+    
+    init(analytics: Analytics) {
+        self.analytics = analytics
+    }
+    
+    func track(event: String) {
+        analytics.log(event: event)
+    }
+}
+
+protocol Analytics {
+    func log(event: String)
+}
+
+class MixpanelSDK: Analytics {
+    var events = [String]()
+        
+    func log(event: String) {
+        events.append(event)
+        print("---event: \(event)---")
+    }
+}
+
 class SingletonIssuesTests: XCTestCase {
+    
+    func test_init_doesNotLogData() {
+        let analytics = AnalyticsSpy()
+        let _ = Tracker(analytics: analytics)
+        
+        XCTAssertEqual(analytics.events.count, 0)
+    }
+    
+    func test_log_capturesData() {
+        let analytics = AnalyticsSpy()
+        let sut = Tracker(analytics: analytics)
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut.track(event: "Button tap")
+
+        XCTAssertEqual(analytics.events.count, 1)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func test_log_capturedDataIsAccurate() {
+        let analytics = AnalyticsSpy()
+        let sut = Tracker(analytics: analytics)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        sut.track(event: "Signed up")
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        XCTAssertEqual(analytics.events, ["Signed up"])
+    }
+    
+    class AnalyticsSpy: Analytics {
+        var events = [String]()
+        
+        func log(event: String) {
+            events.append(event)
         }
+
     }
 
 }
