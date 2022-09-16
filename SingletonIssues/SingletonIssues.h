67db18524ea21243d78494226c6b4bf54ef62c8d
Dylan Sewell <contact@dylanswl.com>
2022-09-16 07:25:23 -0700

    Initial Commit

---
+++ SingletonIssues/SingletonIssues.h
@@ -0,0 +1,18 @@
+//
+//  SingletonIssues.h
+//  SingletonIssues
+//
+//  Created by Dylan Sewell on 9/16/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SingletonIssues.
+FOUNDATION_EXPORT double SingletonIssuesVersionNumber;
+
+//! Project version string for SingletonIssues.
+FOUNDATION_EXPORT const unsigned char SingletonIssuesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SingletonIssues/PublicHeader.h>
+
+
